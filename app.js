5bed8571eb52323599e136710d3b9519484295ff
larab30 <beralara74@gmail.com>
2026-02-16 22:03:57 -0600

    Move index.html to project root

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/calificaciones', (req, res) => {
